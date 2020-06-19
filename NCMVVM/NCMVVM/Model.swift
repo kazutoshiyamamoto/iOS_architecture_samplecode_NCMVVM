43eeb36e01765339710c53dac308b7595b9ce3eb
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-06-19 23:43:51 +0900

    引数の値によって結果を返す関数validateを追加

--- NCMVVM/NCMVVM/Model.swift
+++ NCMVVM/NCMVVM/Model.swift
@@ -24,5 +24,25 @@ protocol ModelProtocol {
 }
 
 final class Model: ModelProtocol {
-    
+    func validate(idText: String?, passwordText: String?) -> Result<Void> {
+        switch (idText, passwordText) {
+        case (.none, .none):
+            return .failure(ModelError.invalidIdAndPassword)
+        case (.none, .some):
+            return .failure(ModelError.invalidId)
+        case (.some, .none):
+            return .failure(ModelError.invalidPassword)
+        case (let idText?, let passwordText?):
+            switch (idText.isEmpty, passwordText.isEmpty) {
+            case (true, true):
+                return .failure(ModelError.invalidIdAndPassword)
+            case (false, false):
+                return .success(())
+            case (true, false):
+                return .failure(ModelError.invalidId)
+            case (false, true):
+                return .failure(ModelError.invalidPassword)
+            }
+        }
+    }
 }
